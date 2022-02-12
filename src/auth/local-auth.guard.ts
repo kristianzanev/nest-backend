4f9a6600d5be13ce1d2c226edcf12f0899459d5c
Kristian Zanev <kristianzanev@hotmail.com>
2022-02-13 00:21:57 +0200

    fix: added local strategy description

--- src/auth/local-auth.guard.ts
+++ src/auth/local-auth.guard.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
+/**
+ * @description this using local.strategy.ts through AuthGuard by passing 'local' param
+ */
 @Injectable()
 export class LocalAuthGuard extends AuthGuard('local') {}
--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -4,6 +4,14 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { User } from '../users/interfaces/user.interface';
 
+/**
+ * @description This authentication strategy (passport-local) is local which means that it's custom and
+ * can be modified by changing the AuthService.
+ * Other strategies for authenticating can be added, like:
+ * passport-facebook
+ * passport-google-oauth
+ * passport-twitter
+ */
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
